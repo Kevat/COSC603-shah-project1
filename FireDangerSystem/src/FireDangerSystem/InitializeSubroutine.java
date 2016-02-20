5c8cc80356bfe260b5abe74c1ce213f91613cf65
Kevat <kevatshah@gmail.com>
2016-02-20 12:07:25 -0500

    Added FireDangerSystem
    Declare Input and output variables

---
+++ FireDangerSystem/src/FireDangerSystem/InitializeSubroutine.java
@@ -0,0 +1,45 @@
+/*
+ * 
+ */
+package FireDangerSystem;
+
+/**
+ * @author Kevat Shah
+ * @Version 1.0
+ */
+/**
+ * @author Kevat Shah
+ * @Version 1.0
+ */
+public class InitializeSubroutine {
+	/**
+	 * The method initializes the fire danger system 
+	 * and serves as the main/start method for the system
+	 *
+	 * @param args - CLI arguments passed in to the system
+	 */
+	public static void main(String[] args) {
+		
+		//Variables to hold input data
+		double dryBulbReading;
+		double wetBulbReading;
+		Boolean snowOnGround;
+		double precedingPrecipitation;
+		double windSpeed;
+		double yesterdaysBuildUp;
+		double herbaceousStage;
+		
+		
+		//Variables to hold output data
+		double dryingFactor;
+		double fineFuelMoisture;
+		double adjustedFuelMoisture;
+		double grassSpreadIndex;
+		double timberSpreadIndex;
+		double fireLoadRating;
+		double buildUpIndex;
+		
+		//Initialize Output Variables
+	}
+
+}
